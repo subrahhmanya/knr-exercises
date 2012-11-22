aa6b1a56d1c20359769c654917890867b8176441
Daniel Campbell <dlcampbell@gmx.com>
2012-11-22 06:42:57 -0600

    Solve exercise 1.20: Detab

---
+++ 1-20_detab.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/* The C Programming Language: 2nd Edition
+ * Exercise 1-20:
+ * "Write a program `detab` that replaces tabs in the input with the proper
+ * number of blanks to space to the next tabstop. Assume a fixed set of
+ * tabstops, say every 'n' columns. Should 'n' be a variable or a symbolic
+ * parameter?
+ *
+ * Answer: 'n' should be a symbolic parameter. It's more apparent what's being
+ * worked with and it's not susceptible to scope. Though, in this simple
+ * program it really doesn't matter.
+ */
+
+#define TABWIDTH 8
+
+int main(void) {
+	int column, c;
+	column = 0;
+	while ((c = getchar()) != EOF) {
+		// Be sure that the character is a tab
+		if (c == '\t') {
+			/*
+			 * Divide a line by TABWIDTH and you have your tabstops. If you
+			 * modulo by TABWIDTH and it equals 0, you've reached a tabstop and
+			 * don't need to output more spaces!
+			 */
+			while (column % TABWIDTH != 0 && column != 0) {
+				putchar(' ');
+				++column;
+			}
+		} else {
+			// non-tabs simply get output.
+			putchar(c);
+			++column;
+			// Line-endings should reset the column counter after being output.
+			if (c == '\n') {
+				column = 0;
+			}
+		}
+	}
+	return 0;
+}
